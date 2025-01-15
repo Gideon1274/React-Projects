d2c3c09c8ca52305a45d8498cd60086f242a0dc3
0xSendo <142055962+0xSendo@users.noreply.github.com>
2025-01-15 21:22:43 +0800

    Add files via upload

---
+++ Color Sequence/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+import ColorGame from './Components/ColorGame';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Color Sequence</h1> {}
+      <ColorGame />
+    </div>
+  );
+}
+
+export default App;
